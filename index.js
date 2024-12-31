f8a4bd6fdecbae138803121aba57ff93ee505eac
Bethania Simangunsong <bethania21ti@mahasiswa.pcr.ac.id>
2024-12-31 15:33:26 +0700

    deploy front-end

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from "express";
 import multer from "multer";
 import { Client } from "@gradio/client";
-import cors from "cors"; // Import CORS middleware
+//import cors from "cors"; // Import CORS middleware
 
 // Initialize Express
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Enable CORS to allow requests from your front-end
-app.use(
-  cors({
-    origin: "http://192.168.90.105:8080", // Replace with your frontend's origin
-    //methods: ["GET", "POST"],
-    //llowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://192.168.90.105:8080", // Replace with your frontend's origin
+//     //methods: ["GET", "POST"],
+//     //llowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 // Set up multer storage and file filtering
 const storage = multer.memoryStorage(); // Store files in memory
 const upload = multer({ storage: storage });
